53e3dfb07a811fdd59ca512d5bfb66f90d68c14b
Christopher de Beer <christopherdebeer@gmail.com>
2025-10-28 13:57:04 +0000

    attempt e2e tests fix (timeout)

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:5173/machine',
+        baseURL: 'http://localhost:5173/machine/',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -72,7 +72,7 @@ export default defineConfig({
     /* Run local dev server before starting the tests */
     webServer: {
         command: 'npm run dev:web',
-        url: 'http://localhost:5173/machine',
+        url: 'http://localhost:5173/machine/',
         reuseExistingServer: !process.env.CI,
         timeout: 120 * 1000, // 2 minutes to start
         stdout: 'pipe',
